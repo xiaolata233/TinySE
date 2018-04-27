fb26f1ebd5f44a810523ddbde98e95704cf64197
Keonwoo kim <kdbml314@gmail.com>
2018-04-27 12:13:19 +0900

    Interface

--- src/main/java/edu/hanyang/indexer/BPlusTree.java
+++ src/main/java/edu/hanyang/indexer/BPlusTree.java
@@ -1,11 +1,13 @@
 package edu.hanyang.indexer;
 
+import java.io.IOException;
+
 public interface BPlusTree {
-	public void open(String filepath, int blocksize, int nblocks);
+	public void open(String filepath, int blocksize, int nblocks) throws IOException;
 
-	public void insert(int key, int val);
+	public void insert(int key, int val) throws IOException;
 
-	public int search(int key);
+	public int search(int key) throws IOException;
 
-	public void close();
+	public void close() throws IOException;
 }
