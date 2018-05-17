8fdeec5f2460dbded108474af48996a279e7c17b
Younghoon Kim <nongaussian@gmail.com>
2018-05-17 12:13:15 +0900

    minor

--- src/main/java/edu/hanyang/indexer/Index.java
+++ src/main/java/edu/hanyang/indexer/Index.java
@@ -1,96 +1,68 @@
 package edu.hanyang.indexer;
 
-import java.io.BufferedOutputStream;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.tuple.MutableTriple;
 
 public class Index {
 	
-	private final String tmppostingsfile = "tmppostings";
+	private BPlusTree tree = null;
 	
-	public String dir = null;
-	public String tmpdir = null;
-	
-	private Tokenizer tokenizer = null;
-	
-	private int current_termid = 0;
-	private TreeMap<String, Integer> termids = new TreeMap<String, Integer> ();
-	
-	public Index (String dir, String tmpdir) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		this.dir = dir;
-		this.tmpdir = tmpdir;
-		
+	public Index () throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		// external code binding
-		Class<?> cls = Class.forName("edu.hanyang.submit.TinySETokenizer");
-		tokenizer = (Tokenizer) cls.newInstance();
-
+		Class<?> cls = Class.forName("edu.hanyang.submit.TinySEBPlusTree");
+		tree = (BPlusTree) cls.newInstance();
 	}
 	
-	private int get_termid (String term) {
-		if (termids.containsKey(term)) {
-			return termids.get(term);
-		}
-		else {
-			termids.put(term, current_termid);
-			return current_termid++;
-		}
-	}
-	
-	public void run () throws IOException {
-		File directory = new File(dir);
-		File[] contents = directory.listFiles();
-		
-		tokenizer.setup();
-		
-		DataOutputStream postings = new DataOutputStream(
-				new BufferedOutputStream (
-						new FileOutputStream (
-								dir + File.separator + tmppostingsfile)));
-		
-		for (File f : contents) {
-			if (f.isFile()) {
-				int docid = Integer.parseInt(f.getName());
-				
-				int pos = 0;
-				try (BufferedReader br = new BufferedReader(new FileReader(f.getAbsolutePath()))) {
-
-					String line = null;
-					while ((line = br.readLine()) != null) {
-						List<String> arr = tokenizer.split(line);
-						
-						for (String term: arr) {
-							int id = get_termid (term);
-							
-							postings.writeInt(id);
-							postings.writeInt(docid);
-							postings.writeInt(pos++);
-						}
-					}
-
-				} catch (IOException e) {
-					System.err.println("[warn] fail to read file: " + f.getAbsolutePath());
-				}
-			}
-		}
-		
-		postings.close();
-		tokenizer.clean();
-	}
-
-	public static void main(String[] args) throws Exception {
-		if (args.length != 2) {
-			System.err.print("usage: <directory to corpus> <temporary directory>");
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+		if (args.length != 6) {
+			System.err.print("usage: <inputfile> <directory metafile> <directory nodefile> <posting file> <block size> <number of available blocks>");
 			System.exit(1);
 		}
 		
-		Index indexer = new Index(args[0], args[1]);
-		indexer.run();
+		int blocksize = Integer.parseInt(args[4]);
+		int nblocks = Integer.parseInt(args[5]);
+		
+		Index index = new Index();
+		index.run(args[0], args[1], args[2], args[3], blocksize, nblocks);
 	}
 
+	private void run(String inputfile, String dirmetafile, String dirnodefile, String postingfile, int blocksize, int nblocks) throws IOException {
+		DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(inputfile)));
+		
+		int[] tuple = new int[3];
+		
+		int cnt = 0;
+		int prev_word = -1;
+		while (read_tuple(input, tuple)) {
+			cnt++;
+		}
+	}
+
+	private boolean read_tuple (DataInputStream in, int[] tuple) throws IOException {
+		try {
+			tuple[0] = in.readInt();
+		}
+		catch (EOFException e) {
+			return false;
+		}
+		
+		try {
+			tuple[1] = in.readInt();
+			tuple[2] = in.readInt();
+		}
+		catch (EOFException e) {
+			throw new IOException("invalid input posting file; wrong alignment");
+		}
+		
+		return true;
+	}
 }
