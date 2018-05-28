a6adef768378d987d0ba03ed3f72c5d8d33a2f13
Younghoon Kim <nongaussian@gmail.com>
2018-05-28 19:23:14 +0900

    file moved

--- src/main/java/edu/hanyang/indexer/IntermediatePositionalList.java
+++ src/main/java/edu/hanyang/indexer/IntermediatePositionalList.java
@@ -1,4 +1,4 @@
-package edu.hanyang.utils;
+package edu.hanyang.indexer;
 
 public abstract class IntermediatePositionalList {
 	public abstract void put_docid_and_pos(int docid, int pos);
--- src/main/java/edu/hanyang/utils/TestIntermediatePositionalList.java
+++ src/main/java/edu/hanyang/utils/TestIntermediatePositionalList.java
@@ -1,5 +1,7 @@
 package edu.hanyang.utils;
 
+import edu.hanyang.indexer.IntermediatePositionalList;
+
 public class TestIntermediatePositionalList extends IntermediatePositionalList {
 	@Override
 	public void put_docid_and_pos(int docid, int pos) {
